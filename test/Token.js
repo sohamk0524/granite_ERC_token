14ccfe7b988beeed8918f292a60bec390b921f52
Divyansh Rajesh Jain <divyanshrajeshjain@divyanshs-mbp.lan>
2021-12-27 02:28:15 -0800

    ERC20 boilerplate code

--- test/Token.js
+++ test/Token.js
@@ -4,7 +4,7 @@ describe("Token contract", function () {
   it("Deployment should assign the total supply of tokens to the owner", async function () {
     const [owner] = await ethers.getSigners();
 
-    const Token = await ethers.getContractFactory("Token");
+    const Token = await ethers.getContractFactory("ERC20");
 
     const hardhatToken = await Token.deploy();
 
