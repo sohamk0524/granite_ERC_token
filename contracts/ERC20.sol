b7208fbe3c06ae86c08e75e9b800dc63f2ae3719
Divyansh Rajesh Jain <divyanshrajeshjain@divyanshs-mbp.lan>
2021-12-27 02:49:20 -0800

    Introduced all functions in interface. Implemented easy functions

--- contracts/ERC20.sol
+++ contracts/ERC20.sol
@@ -18,13 +18,34 @@ contract ERC20 is IERC20 {
 
   mapping (address => mapping (address => uint256)) private _allowed;
 
+  string private _name;
+  string private _symbol;
+	uint8 private _decimals;
   uint256 private _totalSupply;
+
+  /**
+	* @dev Name of the token
+   */
+  function name() public view returns (string memory) {
+		return _name;
+  }
+
+	/**
+	* @dev Symbol of the Token
+	 */
+  function symbol() public view returns (string memory) {
+		return _symbol;
+  }
+
+	function decimals() public view returns (uint8) {
+		return _decimals;
+	}
 
   /**
   * @dev Total number of tokens in existence
   */
   function totalSupply() public view returns (uint256) {
-    return _totalSupply;
+		return _totalSupply;
   }
 
   /**
@@ -33,7 +54,7 @@ contract ERC20 is IERC20 {
   * @return An uint256 representing the amount owned by the passed address.
   */
   function balanceOf(address owner) public view returns (uint256) {
-    return _balances[owner];
+		return _balances[owner];
   }
 
   /**
@@ -42,15 +63,8 @@ contract ERC20 is IERC20 {
    * @param spender address The address which will spend the funds.
    * @return A uint256 specifying the amount of tokens still available for the spender.
    */
-  function allowance(
-    address owner,
-    address spender
-   )
-    public
-    view
-    returns (uint256)
-  {
-    return _allowed[owner][spender];
+  function allowance(address owner, address spender) public view returns (uint256) {
+		return _allowed[owner][spender];
   }
 
   /**
@@ -59,13 +73,13 @@ contract ERC20 is IERC20 {
   * @param value The amount to be transferred.
   */
   function transfer(address to, uint256 value) public returns (bool) {
-    require(value <= _balances[msg.sender]);
-    require(to != address(0));
+		require(value <= _balances[msg.sender]);
+		require(to != address(0));
 
-    _balances[msg.sender] = _balances[msg.sender].sub(value);
-    _balances[to] = _balances[to].add(value);
-    emit Transfer(msg.sender, to, value);
-    return true;
+		_balances[msg.sender] = _balances[msg.sender].sub(value);
+		_balances[to] = _balances[to].add(value);
+		emit Transfer(msg.sender, to, value);
+		return true;
   }
 
   /**
@@ -78,11 +92,11 @@ contract ERC20 is IERC20 {
    * @param value The amount of tokens to be spent.
    */
   function approve(address spender, uint256 value) public returns (bool) {
-    require(spender != address(0));
+		require(spender != address(0));
 
-    _allowed[msg.sender][spender] = value;
-    emit Approval(msg.sender, spender, value);
-    return true;
+		_allowed[msg.sender][spender] = value;
+		emit Approval(msg.sender, spender, value);
+		return true;
   }
 
   /**
@@ -91,23 +105,16 @@ contract ERC20 is IERC20 {
    * @param to address The address which you want to transfer to
    * @param value uint256 the amount of tokens to be transferred
    */
-  function transferFrom(
-    address from,
-    address to,
-    uint256 value
-  )
-    public
-    returns (bool)
-  {
-    require(value <= _balances[from]);
-    require(value <= _allowed[from][msg.sender]);
-    require(to != address(0));
+  function transferFrom( address from, address to, uint256 value) public returns (bool) {
+		require(value <= _balances[from]);
+		require(value <= _allowed[from][msg.sender]);
+		require(to != address(0));
 
-    _balances[from] = _balances[from].sub(value);
-    _balances[to] = _balances[to].add(value);
-    _allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
-    emit Transfer(from, to, value);
-    return true;
+		_balances[from] = _balances[from].sub(value);
+		_balances[to] = _balances[to].add(value);
+		_allowed[from][msg.sender] = _allowed[from][msg.sender].sub(value);
+		emit Transfer(from, to, value);
+		return true;
   }
 
   /**
@@ -119,19 +126,11 @@ contract ERC20 is IERC20 {
    * @param spender The address which will spend the funds.
    * @param addedValue The amount of tokens to increase the allowance by.
    */
-  function increaseAllowance(
-    address spender,
-    uint256 addedValue
-  )
-    public
-    returns (bool)
-  {
-    require(spender != address(0));
-
-    _allowed[msg.sender][spender] = (
-      _allowed[msg.sender][spender].add(addedValue));
-    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
-    return true;
+  function increaseAllowance( address spender, uint256 addedValue) public returns (bool) {
+		require(spender != address(0));
+		_allowed[msg.sender][spender] = (_allowed[msg.sender][spender].add(addedValue));
+		emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+		return true;
   }
 
   /**
@@ -143,19 +142,11 @@ contract ERC20 is IERC20 {
    * @param spender The address which will spend the funds.
    * @param subtractedValue The amount of tokens to decrease the allowance by.
    */
-  function decreaseAllowance(
-    address spender,
-    uint256 subtractedValue
-  )
-    public
-    returns (bool)
-  {
-    require(spender != address(0));
-
-    _allowed[msg.sender][spender] = (
-      _allowed[msg.sender][spender].sub(subtractedValue));
-    emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
-    return true;
+  function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool) {
+		require(spender != address(0));
+		_allowed[msg.sender][spender] = (_allowed[msg.sender][spender].sub(subtractedValue));
+		emit Approval(msg.sender, spender, _allowed[msg.sender][spender]);
+		return true;
   }
 
   /**
@@ -166,10 +157,10 @@ contract ERC20 is IERC20 {
    * @param amount The amount that will be created.
    */
   function _mint(address account, uint256 amount) internal {
-    require(account != address(0x0));
-    _totalSupply = _totalSupply.add(amount);
-    _balances[account] = _balances[account].add(amount);
-    emit Transfer(address(0), account, amount);
+		require(account != address(0x0));
+		_totalSupply = _totalSupply.add(amount);
+		_balances[account] = _balances[account].add(amount);
+		emit Transfer(address(0), account, amount);
   }
 
   /**
@@ -179,12 +170,12 @@ contract ERC20 is IERC20 {
    * @param amount The amount that will be burnt.
    */
   function _burn(address account, uint256 amount) internal {
-    require(account != address(0x0));
-    require(amount <= _balances[account]);
+		require(account != address(0x0));
+		require(amount <= _balances[account]);
 
-    _totalSupply = _totalSupply.sub(amount);
-    _balances[account] = _balances[account].sub(amount);
-    emit Transfer(account, address(0), amount);
+		_totalSupply = _totalSupply.sub(amount);
+		_balances[account] = _balances[account].sub(amount);
+		emit Transfer(account, address(0), amount);
   }
 
   /**
@@ -195,12 +186,11 @@ contract ERC20 is IERC20 {
    * @param amount The amount that will be burnt.
    */
   function _burnFrom(address account, uint256 amount) internal {
-    require(amount <= _allowed[account][msg.sender]);
+		require(amount <= _allowed[account][msg.sender]);
 
-    // Should https://github.com/OpenZeppelin/zeppelin-solidity/issues/707 be accepted,
-    // this function needs to emit an event with the updated approval.
-    _allowed[account][msg.sender] = _allowed[account][msg.sender].sub(
-      amount);
-    _burn(account, amount);
+		// Should https://github.com/OpenZeppelin/zeppelin-solidity/issues/707 be accepted,
+		// this function needs to emit an event with the updated approval.
+		_allowed[account][msg.sender] = _allowed[account][msg.sender].sub(amount);
+		_burn(account, amount);
   }
 }
--- contracts/IERC20.sol
+++ contracts/IERC20.sol
@@ -6,6 +6,12 @@ pragma solidity ^0.8.11;
  * @dev see https://github.com/ethereum/EIPs/issues/20
  */
 interface IERC20 {
+  function name() external view returns (string memory);
+
+  function symbol() external view returns (string memory);
+
+  function decimals() external view returns (uint8);
+
   function totalSupply() external view returns (uint256);
 
   function balanceOf(address who) external view returns (uint256);
